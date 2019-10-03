f686e0f985f6fdcbe5866ae006925eea3e0c39a7
Alexis <alexis.morin.mail@gmail.com>
2019-10-03 01:36:23 -0400

    mvp

--- Assets/Spraycan.cs
+++ Assets/Spraycan.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Es.InkPainter;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR;
-using Es.InkPainter;
 
-public class Spraycan : MonoBehaviour
-{
+public class Spraycan : MonoBehaviour {
 
     [SerializeField]
     private Brush brush;
@@ -18,30 +17,38 @@ public class Spraycan : MonoBehaviour
     MeshRenderer meshRenderer;
     [SerializeField]
     AudioSource audioSource;
+
     [SerializeField]
-    CanvasReloader reloadScript;
+    GameObject otherCan;
 
     [SerializeField]
     bool isSpraying = true;
 
-    void Start()
-    {
-        XRDevice.SetTrackingSpaceType(TrackingSpaceType.RoomScale);
+    [SerializeField]
+    string sceneToLoad;
+
+    float currentHue = 0.99f;
+
+    void Start () {
+        XRDevice.SetTrackingSpaceType (TrackingSpaceType.RoomScale);
     }
 
-    void UpdateColorSelection(bool black, bool white, bool color, float hue)
-    {
-        if (black)
-        {
+    void ChangeValue () {
+        currentHue -= 0.33f;
+        if (currentHue <= 0f) {
+            currentHue = 0.99f;
+        }
+    }
+
+    void UpdateColorSelection (bool black, bool white, bool color, float hue) {
+        if (black) {
             brush.Color = Color.black;
         }
-        if (white)
-        {
+        if (white) {
             brush.Color = Color.white;
         }
-        if (color)
-        {
-            Color newColor = Color.HSVToRGB(hue / 360f, 1f, 1f);
+        if (color) {
+            Color newColor = Color.HSVToRGB (hue / 360f, 0.9f, currentHue);
             brush.Color = newColor;
         }
 
@@ -51,72 +58,64 @@ public class Spraycan : MonoBehaviour
         newMaterials[1].color = brush.Color;
         meshRenderer.materials = newMaterials;
 
-
     }
 
+    void Toggle () {
+        otherCan.SetActive (true);
+        gameObject.SetActive (false);
+    }
 
-    void Update()
-    {
+    void Update () {
 
-        if (Input.GetButtonDown("XRI_Left_MenuButton"))
-        {
-            Scene scene = SceneManager.GetActiveScene();
-            SceneManager.LoadScene(scene.name, LoadSceneMode.Single);
+        if (Input.GetButtonDown ("XRI_Left_MenuButton")) {
+            Scene scene = SceneManager.GetActiveScene ();
+            SceneManager.LoadScene (sceneToLoad, LoadSceneMode.Single);
         }
 
-        audioSource.volume = Input.GetAxis("XRI_Left_Trigger") + Input.GetAxis("XRI_Right_Trigger");
+        float currentWheelSelectionHorizontal = Input.GetAxis ("XRI_Left_Primary2DAxis_Horizontal") + Input.GetAxis ("XRI_Right_Primary2DAxis_Horizontal");
+        float currentWheelSelectionVertical = Input.GetAxis ("XRI_Left_Primary2DAxis_Vertical") + Input.GetAxis ("XRI_Right_Primary2DAxis_Vertical");
 
-        float currentWheelSelectionHorizontal = Input.GetAxis("XRI_Left_Primary2DAxis_Horizontal") + Input.GetAxis("XRI_Right_Primary2DAxis_Horizontal");
-        float currentWheelSelectionVertical = Input.GetAxis("XRI_Left_Primary2DAxis_Vertical") + Input.GetAxis("XRI_Right_Primary2DAxis_Vertical");
-
-        if (currentWheelSelectionHorizontal != 0.0f || currentWheelSelectionVertical != 0.0f)
-        {
-            float currentRotation = Mathf.Atan2(currentWheelSelectionVertical, currentWheelSelectionHorizontal) * Mathf.Rad2Deg;
-            UpdateColorSelection(false, false, true, currentRotation);
+        if (currentWheelSelectionHorizontal != 0.0f || currentWheelSelectionVertical != 0.0f) {
+            float currentRotation = Mathf.Atan2 (currentWheelSelectionVertical, currentWheelSelectionHorizontal) * Mathf.Rad2Deg;
+            UpdateColorSelection (false, false, true, currentRotation);
         }
 
+        if (Input.GetButtonDown ("XRI_Left_Primary2DAxisClick") || Input.GetButtonDown ("XRI_Right_Primary2DAxisClick")) {
+            Toggle ();
+        }
 
+        if (Input.GetButtonDown ("XRI_Left_PrimaryButton") || Input.GetButtonDown ("XRI_Right_PrimaryButton")) {
+            ChangeValue ();
+        }
+        if (Input.GetButtonDown ("XRI_Left_SecondaryButton") || Input.GetButtonDown ("XRI_Right_SecondaryButton")) {
+            UpdateColorSelection (false, true, false, 0f);
+        }
 
-        if (Input.GetAxis("XRI_Left_Trigger") > 0.5f || Input.GetAxis("XRI_Right_Trigger") > 0.5f)
-        {
+        if (Input.GetAxis ("XRI_Left_Trigger") > 0.5f || Input.GetAxis ("XRI_Right_Trigger") > 0.5f) {
             isSpraying = true;
         }
-        if (Input.GetAxis("XRI_Left_Trigger") < 0.5f && Input.GetAxis("XRI_Right_Trigger") < 0.5f)
-        {
+        if (Input.GetAxis ("XRI_Left_Trigger") < 0.5f && Input.GetAxis ("XRI_Right_Trigger") < 0.5f) {
             isSpraying = false;
-        }
-        if (Input.GetButtonDown("XRI_Left_PrimaryButton") || Input.GetButtonDown("XRI_Right_PrimaryButton"))
-        {
-            UpdateColorSelection(true, false, false, 0f);
-        }
-        if (Input.GetButtonDown("XRI_Left_SecondaryButton") || Input.GetButtonDown("XRI_Right_SecondaryButton"))
-        {
-            UpdateColorSelection(false, true, false, 0f);
+            audioSource.volume = 0f;
         }
 
-        if (isSpraying)
-        {
-
+        if (isSpraying) {
 
             RaycastHit hitInfo;
-            if (Physics.Raycast(sprayPosition.transform.position, sprayPosition.forward, out hitInfo, 2f))
-            {
-                var paintObject = hitInfo.transform.GetComponent<InkCanvas>();
-                if (paintObject != null)
-                {
-                    sprayParticleSystem.Emit(5);
-                    brush.RotateAngle = Random.Range(0f, 180f);
+            if (Physics.Raycast (sprayPosition.transform.position, sprayPosition.forward, out hitInfo, 2f)) {
+                var paintObject = hitInfo.transform.GetComponent<InkCanvas> ();
+                if (paintObject != null) {
+                    audioSource.volume = Input.GetAxis ("XRI_Left_Trigger") + Input.GetAxis ("XRI_Right_Trigger");
+                    sprayParticleSystem.Emit (5);
+                    brush.RotateAngle = Random.Range (0f, 180f);
                     brush.Scale = (0.02f / 0.42f) * hitInfo.distance;
-                    paintObject.Paint(brush, hitInfo);
+                    paintObject.Paint (brush, hitInfo);
 
                 }
 
-
             }
 
-
         }
-
 
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# zut-spraypainter
- VR Spray Painting
+# Spraypainter
+## Oculus Quest Graffiti App
+
+![header](images/header.png)
+
+While I wait for an eventual Oculus Quest [Kingspray](http://infectiousape.com/) port, here's something I whipped up in a few hours to tide myself over.
+
+![gif](images/gif.gif)
+
+### Control Scheme
+- Trigger: Spray!
+- Joystick : Select Color from Color Wheel
+- X/A Button: Change Hue of Color Wheel
+- B/Y Button: Turn Paint White
+- Menu Button: Switch Between Door and Brickwall scenes / Reset Map
+- Right Joystick (Press): Switch handedness
+
+![install](images/install.png)
+
+### Install Process
+Just grab the APK from the releases and drag it into SideQuest, nothing else required!
+
+
